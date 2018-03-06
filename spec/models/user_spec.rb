3768faa58911a03e1086b3e4e5ca10ac729d1fc7
SlickJohnson <liwa.johnson@gmail.com>
2018-03-05 17:42:37 -0800

    Add association validation to User rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with attributes" do
-    user = User.new(name: "Eliel", email: "eliel@test.com")
-    expect(user).to be_valid
+  describe "Validations" do
+    it "is valid with attributes" do
+      user = User.new(name: "Eliel", email: "eliel@test.com")
+      expect(user).to be_valid
+    end
+
+    it "is invalid without a name" do
+      bad_user = User.new(name: nil, email: "test@mail.com")
+      expect(bad_user).to_not be_valid
+    end
+
+    it "is invalid without an email" do
+      bad_user = User.new(name: "Eliel", email: nil)
+      expect(bad_user).to_not be_valid
+    end
   end
 
-  it "is invalid without a name" do
-    bad_user = User.new(name: nil, email: "test@mail.com")
-    expect(bad_user).to_not be_valid
-  end
-
-  it "is invalid without an email" do
-    bad_user = User.new(name: "Eliel", email: nil)
-    expect(bad_user).to_not be_valid
-  end
+  describe "Associations" do
+    it "should have many memos" do
+      assoc = User.reflect_on_association(:memos)
+      expect(assoc).to eq :has_many
+    end
 end
