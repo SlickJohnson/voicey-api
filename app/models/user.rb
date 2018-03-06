7fcad440b4fc34c9e887bd4d3cb93917047d6cd7
SlickJohnson <liwa.johnson@gmail.com>
2018-03-06 11:59:41 -0800

    Add functions to user.rb to help with User authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,36 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include BCrypt
   has_many :memos
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  attribute :password, :string
+  before_save :encrypt_password
+  before_create :generate_token
+
+  # Check if user is authenticated
+  def self.authenticate(email, password)
+    user = self.find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  # Salt and hash user's password
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  # Generate a token for a user
+  def generate_token
+    token_gen = SecureRandom.hex
+    self.token = token_gen
+    token_gen
+  end
 end
