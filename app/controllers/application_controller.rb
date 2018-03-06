98a57afbc3bfd52e19d2696b056b3206c35a9779
SlickJohnson <liwa.johnson@gmail.com>
2018-03-06 12:17:43 -0800

    Add authentication to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  # Require authentication for all controllers
+  before_action :require_login
+
+  # Authenticate user
+  def require_login
+    authorize_request || render_unauthorized("Access denied")
+  end
+
+  # Get the current user in a request
+  def current_user
+    @current_user ||= authorize_request
+  end
+
+  # Render a message when user is unauthorized
+  def render_unauthorized(message)
+    errors = { errors: [ { detail: message } ] }
+    render json: errors, status: :unauthorized
+  end
+
+  private
+  # Authenticate user by token
+  def authorize_request
+    authenticate_with_http_token do |token, options|
+      User.find_by(token: token)
+    end
+  end
 end
