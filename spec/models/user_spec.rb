e558294637c74db72a32af73e361e4a93a8b40b6
SlickJohnson <liwa.johnson@gmail.com>
2018-03-07 21:47:35 -0800

    Write tests to check to account for password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,23 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it "is valid with attributes" do
-      user = User.new(name: "Eliel", email: "eliel@test.com")
+      user = User.new(name: "Will", email: "will@test.com", password: "akldjfls")
       expect(user).to be_valid
     end
 
     it "is invalid without a name" do
-      bad_user = User.new(name: nil, email: "test@mail.com")
+      bad_user = User.new(name: nil, email: "test@mail.com", password: "akldjfls")
       expect(bad_user).to_not be_valid
     end
 
     it "is invalid without an email" do
-      bad_user = User.new(name: "Eliel", email: nil)
+      bad_user = User.new(name: "Will", email: nil)
       expect(bad_user).to_not be_valid
     end
+
+    it "is invalid without a password" do
+      bad_user = User.new(name: "Will", email: "test@mail.com", password: nil)
+      expect(bad_user).to_not be_valid
   end
 
   describe "Associations" do
